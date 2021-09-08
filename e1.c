1d74200493248b1f459b083d36d5f79ba41c58e4
LXYing <86389859+XIAOYING96@users.noreply.github.com>
2021-09-08 14:51:07 +0800

    Lecturer

---
+++ e1.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//#pragma warning(disable:4996)
+//
+//void main() {
+//	FILE* fptr;
+//
+//	fptr = fopen("result.txt", "w");
+//	if (fptr = NULL) {
+//		printf("Can't open the file result.txt !");
+//		exit(-1);
+//	}
+//	fprintf(fptr, "ABC\n");
+//	fprintf(fptr, "3");
+//	fclose(fptr);
+//	printf("done");
+//}
